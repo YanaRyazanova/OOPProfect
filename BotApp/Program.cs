1d627a58b0a00fe3ed288225b6ea44d06e68e3c5
mbarakovskiy <mbarakovskiy@gmail.com>
2020-11-07 23:36:54 +0500

    Add Bot Functional for Telegram

--- BotApp/Program.cs
+++ BotApp/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace BotApp
 {
     class Program
     {
+        private static TelegramBotClient client;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var token = "1454646239:AAGi5CFWo0pF7Vlg-j01A4w-jDZEiLA5LA4";
+            // token, который вернул BotFather
+            client = new TelegramBotClient(token);
+            client.OnMessage += BotOnMessageReceived;
+            client.OnMessageEdited += BotOnMessageReceived;
+            client.StartReceiving();
+            Console.ReadLine();
+            client.StopReceiving();
+        }
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+            if (message?.Type == MessageType.Text)
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, message.Text);
+            }
         }
     }
 }
