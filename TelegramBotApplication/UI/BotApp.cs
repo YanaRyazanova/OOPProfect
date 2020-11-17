00ec9f21da5a6e43b6a48c56b5bfb7df828f924a
mbarakovskiy <mbarakovskiy@gmail.com>
2020-11-17 11:46:25 +0500

    some changes

--- TelegramBotApplication/UI/BotApp.cs
+++ TelegramBotApplication/UI/BotApp.cs
@@ -10,9 +10,10 @@ namespace OOPProject
 {
     class BotApp
     {
-        private static readonly List<long> usersList = new List<long>();
+        private static readonly List<long> UsersList = new List<long>();
         private static TelegramBotClient client;
-        private static readonly string token = "1443567108:AAEh-njifk9sV2UAASpPJeNF2Jbu8zZ6nUs"; // token, который вернул BotFather
+        private const string token = "1443567108:AAEh-njifk9sV2UAASpPJeNF2Jbu8zZ6nUs"; // token, который вернул BotFather
+
         static void Main(string[] args)
         {
             client = new TelegramBotClient(token);
@@ -28,8 +29,8 @@ namespace OOPProject
         {
             var message = messageEventArgs.Message;
             var chatId = message.Chat.Id;
-            if (!usersList.Contains(chatId))
-                usersList.Add(chatId);
+            if (!UsersList.Contains(chatId))
+                UsersList.Add(chatId);
             if (message.Type == MessageType.Text)
             {
                 await client.SendTextMessageAsync(chatId, message.Text);
@@ -38,7 +39,7 @@ namespace OOPProject
 
         private static async void BotNotificationsSender(object sender, MessageEventArgs messageEventArgs)
         {
-            foreach (var id in usersList)
+            foreach (var id in UsersList)
             {
                 var message = new Bot.Domain.Functions.LessonReminder("ФТ-201").Do();
                 if (message != null)
