c129b13b38747f0a7cb78e2c6b48383276a599bf
artamaney <artemiyrogov1@gmail.com>
2020-12-03 22:14:05 +0500

    infrastructure changes

--- Infrastructure/DataBaseParser.cs
+++ Infrastructure/DataBaseParser.cs
@@ -6,21 +6,18 @@ using System.Collections.Generic;
 
 namespace Infrastructure
 {
-    public class DataBaseParser
+    public class DataBaseParser : IDataBase
     {
         private readonly DBNameProvider dbNameProvider;
 
-        //public DataBaseParser(DBNameProvider dbNameProvider)
-        //{
-        //    this.dbNameProvider = dbNameProvider;
-        //}
-
-        public string GetTimetableForGroupForCurrentDay(string groupName, DateTime day)
+        public DataBaseParser(DBNameProvider dbNameProvider)
         {
-            //var dbName = dbNameProvider.GetDBName("TimeTable");
-            var path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            path = path.Remove(path.Length - 28);
-            var dbName = "PeopleAndGroup.db";
+            this.dbNameProvider = dbNameProvider;
+        }
+
+        public Lesson[] GetTimetableForGroupForCurrentDay(string groupName, DateTime day)
+        {
+            var dbName = dbNameProvider.GetDBName("TimeTable");
             var days = new Dictionary<string, string>
             {
                 ["Monday"] = "Понедельник",
@@ -33,17 +30,34 @@ namespace Infrastructure
 
             var connection = new SQLiteConnection(string.Format("Data Source={0};", dbName));
             connection.Open();
-            var timetable = new Dictionary<string, string>();
             var command = new SQLiteCommand(string.Format("SELECT timetable FROM TimeTable WHERE group_='{0}' AND dayOfWeek='{1}'",
                 groupName, days[day.DayOfWeek.ToString()]), connection);
             var reader = command.ExecuteReader();
             foreach (DbDataRecord record in reader)
             {
-                var result = record["timetable"].ToString();
+                var timetableString = record["timetable"].ToString();
                 connection.Close();
-                return result;
+                return ParseTimeTable(timetableString, day);
             }
-            return "";
+            return new Lesson[0];
+        }
+
+        public Lesson[] ParseTimeTable(string timetable, DateTime day)
+        {
+            var result = new List<Lesson>();
+            if (timetable.Length > 1)
+            {
+                var rows = timetable.Split('\n');
+                foreach (var row in rows)
+                {
+                    var timeAndOtherTimesAndProgram = row.Split(':');
+                    result.Add(new Lesson(new DateTime(day.Year, day.Month, day.Day,
+                        int.Parse(timeAndOtherTimesAndProgram[0]),
+                        int.Parse(timeAndOtherTimesAndProgram[1].Split()[0]), 0),
+                        timeAndOtherTimesAndProgram[timeAndOtherTimesAndProgram.Length - 1].Trim()));
+                }
+            }
+            return result.ToArray();
         }
     }
 }
