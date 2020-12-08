1b2516c752296bc8d4eec13df48daf2b3935fc93
artamaney <artemiyrogov1@gmail.com>
2020-12-08 10:34:39 +0500

    json db has been added

---
+++ Infrastructure/Item.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    class Item
+    {
+        [JsonProperty("dayOfWeekAndGroup")]
+        public string dayOfWeekAndGroup { get; set; }
+    }
+}
