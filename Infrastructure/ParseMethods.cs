c2ed770793e42fddd46cf64cda3a43d7b0571d5d
artamaney <artemiyrogov1@gmail.com>
2020-12-11 15:38:20 +0500

    CSV parser has been added

---
+++ Infrastructure/ParseMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    class ParseMethods
+    {
+        public Lesson[] ParseTimeTable(string timetable, DateTime day)
+        {
+            var result = new List<Lesson>();
+            if (timetable.Length > 1)
+            {
+                var rows = timetable.Split('\n');
+                foreach (var row in rows)
+                {
+                    var timeAndOtherTimesAndProgram = row.Split(':');
+                    result.Add(new Lesson(new DateTime(day.Year, day.Month, day.Day,
+                        int.Parse(timeAndOtherTimesAndProgram[0]),
+                        int.Parse(timeAndOtherTimesAndProgram[1].Split()[0]), 0),
+                        timeAndOtherTimesAndProgram[timeAndOtherTimesAndProgram.Length - 1].Trim()));
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
