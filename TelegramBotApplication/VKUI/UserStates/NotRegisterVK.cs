a8d66f8295db1d598c1ccfaa5602c1bc20745563
mbarakovskiy <mbarakovskiy@gmail.com>
2021-01-10 14:06:39 +0500

    check

--- TelegramBotApplication/VKUI/UserStates/NotRegisterVK.cs
+++ TelegramBotApplication/VKUI/UserStates/NotRegisterVK.cs
@@ -71,7 +71,7 @@ namespace View
                 case "начать":
                     {
                         var text = new MessageResponse(ResponseType.Start).response;
-                        peopleParser.AddNewUser(user.UserId);
+                        peopleParser.AddNewUser(user.UserId, "0","vk");
                         peopleParser.EvaluateState(user.UserId);
                         var updatedState = new RegisterInProcessVK(messageHandler, vkMessageSender, peopleParser, vkUnknownMessageProcessor, groupProvider, registerCommandListProvider, addingLinkCommandListProvider);
                         vkMessageSender.SendNotification(user, text, updatedState.GetKeyboard());
